6417dc0592144df9fbb6f4cfbdfc84c71fec8f1f
Abdulsamet Kilincarslan <passmanagerapplication@gmail.com>
2023-10-08 14:13:28 +0300

    master *add architecture

---
+++ app/src/main/java/com/akilincarslan/askmeai/constants/NetworkConstant.kt
@@ -0,0 +1,17 @@
+package com.akilincarslan.askmeai.constants
+
+class NetworkConstant {
+    companion object {
+        const val HTTP_CALL_TIMEOUT_SECOND = 60
+        const val HTTP_CONNECTION_TIMEOUT_SECONDS = 60
+        const val HTTP_READ_TIMEOUT_SECONDS = 120
+    }
+
+    object Endpoint {
+        private const val ENDPOINT_API = "/v1"
+
+        object Home {
+            const val PROFILE = "$ENDPOINT_API/profile"
+        }
+    }
+}
